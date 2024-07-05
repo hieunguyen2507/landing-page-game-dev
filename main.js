4f1827c5a80d14520947ff28d03a3903ccd1b8cc
hieunguyen2507 <148256280+hieunguyen2507@users.noreply.github.com>
2024-07-05 14:45:02 +0700

    update img

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const nav = document.querySelector("nav");
-window.addEventListener("scroll", ()=>{
-    if (window.pageYOffset > 100) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
+const mobileNav = document.querySelector("nav.mobile-nav");
+
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 60) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+  if (window.pageYOffset > 5) {
+    mobileNav.classList.add("scrolled");
+  } else {
+    mobileNav.classList.remove("scrolled");
+  }
+});
+
+menuIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.remove("active");
 });
